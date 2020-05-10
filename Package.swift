153e04279a3e016305127deb25b7fe1106bade63
grey280 <greypatterson@gmail.com>
2020-05-09 22:34:18 -0700

    OpenCombine test?

--- Package.swift
+++ Package.swift
@@ -1,19 +1,24 @@
 // swift-tools-version:5.2
 import PackageDescription
 
+let dependencies: [Package.Dependency] = [
+    // 💧 A server-side Swift web framework.
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
+    .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
+    .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc"),
+    .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc"),
+    .package(name: "Ink", url: "https://github.com/johnsundell/ink.git", from: "0.1.0"),
+]
+#if os(Linux)
+dependencies.append(.package(url: "https://github.com/broadwaylamb/OpenCombine.git", from: "0.8.0"))
+#endif
+
 let package = Package(
     name: "reversi",
     platforms: [
        .macOS(.v10_15)
     ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0-rc"),
-        .package(name: "Ink", url: "https://github.com/johnsundell/ink.git", from: "0.1.0")
-    ],
+    dependencies: dependencies,
     targets: [
         .target(name: "App", dependencies: [
             .product(name: "Fluent", package: "fluent"),
