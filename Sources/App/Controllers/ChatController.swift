4f1e357c8439063c1c76ed881f3ec9696563c0f0
grey280 <greypatterson@gmail.com>
2020-05-09 20:17:40 -0700

    type-safe commands, dangit!

--- Sources/App/Controllers/ChatController.swift
+++ Sources/App/Controllers/ChatController.swift
@@ -13,5 +13,50 @@ struct ChatController {
         ws.onClose.whenComplete { (res) in
             print("Socket disconnected")
         }
+        ws.onText { (ws, text) in
+            <#code#>
+        }
     }
+    
+    enum ChatCommand {
+        case joinRoom(room: String, username: String)
+    }
+    
+    
 }
+
+
+extension ChatController.ChatCommand: Codable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let type = try container.decode(CommandType.self, forKey: .command)
+        switch type {
+        case .joinRoom:
+            let room = try container.decode(String.self, forKey: .room)
+            let username = try container.decode(String.self, forKey: .username)
+            self = .joinRoom(room: room, username: username)
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case .joinRoom(let room, let username):
+            try container.encode(CommandType.joinRoom.rawValue, forKey: .command)
+            try container.encode(room, forKey: .room)
+            try container.encode(username, forKey: .username)
+        }
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case command
+        case room
+        case username
+    }
+    
+    enum CommandType: String {
+        case joinRoom
+    }
+    
+}
+extension ChatController.ChatCommand.CommandType: Codable { }
