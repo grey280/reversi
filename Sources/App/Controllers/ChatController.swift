fde76c2542264088d752b77b92d64b65de5e56a4
grey280 <greypatterson@gmail.com>
2020-05-09 20:21:10 -0700

    Helper function for decoding command

--- Sources/App/Controllers/ChatController.swift
+++ Sources/App/Controllers/ChatController.swift
@@ -8,13 +8,28 @@
 import Vapor
 
 struct ChatController {
+    private let decoder = JSONDecoder()
+    
     func socket(_ req: Request, _ ws: WebSocket) -> () {
         print("Socket connected")
-        ws.onClose.whenComplete { (res) in
+         ws.onClose.whenComplete { (res) in
             print("Socket disconnected")
         }
         ws.onText { (ws, text) in
-            <#code#>
+            guard let command = self.parseCommand(text) else {
+                return
+            }
+            
+        }
+    }
+    
+    private func parseCommand(_ text: String) -> ChatCommand? {
+        do {
+            let jsonData = Data(text.utf8)
+            return try decoder.decode(ChatCommand.self, from: jsonData)
+        } catch {
+            print(error.localizedDescription)
+            return nil
         }
     }
     
