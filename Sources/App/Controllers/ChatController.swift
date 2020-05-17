6ef55722b80923a01a96e7d67f47748f692834dd
grey280 <greypatterson@gmail.com>
2020-05-17 10:35:15 -0700

    Apparently Vapor shares a single ChatController class for all WSes?

--- Sources/App/Controllers/ChatController.swift
+++ Sources/App/Controllers/ChatController.swift
@@ -17,14 +17,9 @@ class ChatController {
     
     private var subscriptions: [AnyCancellable] = []
     
-    // Session variables
-    var user: ChatUser? = nil
-//    var userName: String? = nil
-    var joinedChatRoom: String? = nil
-    
-    private func onDisconnect(_ res: Result<Void, Error>) {
+    private func onDisconnect(_ res: Result<Void, Error>, user: inout ChatUser?, roomName: inout String?) {
         print("Socket disconnected")
-        if let roomName = joinedChatRoom, let user = user {
+        if let roomName = roomName, let user = user {
             guard let room = ChatController.rooms[roomName] else {
                 print("User \(user) was in room \(roomName), but room was not found")
                 return
@@ -39,22 +34,9 @@ class ChatController {
             print("Unable to determine room or user name; nothing to do")
         }
     }
-    private func routeCommand(_ ws: WebSocket, _ text: String){
-        print("Command received")
-        guard let command = self.parseCommand(text) else {
-            print("Unable to parse command.")
-            return
-        }
-        switch command {
-        case .joinRoom(let room, let username):
-            self.joinRoom(room: room, username: username, ws: ws)
-        case .sendMessage(let message):
-            self.sendMessage(message, ws: ws)
-        }
-    }
     
-    private func sendMessage(_ message: String, ws: WebSocket){
-        guard let user = self.user, let roomName = self.joinedChatRoom, let room = ChatController.rooms[roomName] else {
+    private func sendMessage(_ message: String, ws: WebSocket, user: inout ChatUser?, roomName: inout String?){
+        guard let user = user, let roomName = roomName, let room = ChatController.rooms[roomName] else {
             print("User attempted to send a message without first joining a room.")
             return
         }
@@ -79,49 +61,62 @@ class ChatController {
         }
     }
     
-    private func joinRoom(room: String, username: String, ws: WebSocket){
-        if (username == "System"){
-            print("Chat: user attempted to join with illegal username 'System'")
-            return
-        }
-        print("Chat: \(username) requested to join \(room)")
-        let chatRoom: ChatRoom
-        if ChatController.rooms[room] == nil {
-            ChatController.rooms[room] = ChatRoom()
-        }
-        chatRoom = ChatController.rooms[room]!
-        let asUser = ChatUser(username)
-        chatRoom.queue.send(.userJoined(user: asUser))
-        let subscription = chatRoom.queue.sink(receiveValue: { (event) in
-            switch (event){
-            case .privateMessage(let fromUser, let toUser, _):
-                guard toUser == self.user || fromUser == self.user, let res = self.codableAsString(event) else {
-                    return // not for you!
-                }
-                print("Chat: sending private message to \(username)")
-                ws.send(res)
-            default:
-                print("Chat: sending event to \(username)")
-                if let res = self.codableAsString(event) {
-                    ws.send(res)
-                }
-            }
-        })
-        self.subscriptions.append(subscription)
-        let result: JoinRoomResponse = .success(room: room, username: username, membership: chatRoom.userCount)
-        guard let asString = self.codableAsString(result) else {
-            return
-        }
-        ws.send(asString)
-        self.user = asUser
-        chatRoom.users.insert(asUser)
-        self.joinedChatRoom = room
-    }
     
     func socket(_ req: Request, _ ws: WebSocket) -> () {
         print("Socket connected")
-        ws.onClose.whenComplete(onDisconnect(_:))
-        ws.onText(routeCommand(_:_:))
+        var user: ChatUser? = nil
+        var inRoom: String? = nil
+        ws.onClose.whenComplete { (res) in
+            self.onDisconnect(res, user: &user, roomName: &inRoom)
+        }
+        ws.onText { (ws, text) in
+            print("Command received")
+            guard let command = self.parseCommand(text) else {
+                print("Unable to parse command.")
+                return
+            }
+            switch command {
+            case .joinRoom(let room, let username):
+                if (username == "System"){
+                    print("Chat: user attempted to join with illegal username 'System'")
+                    return
+                }
+                print("Chat: \(username) requested to join \(room)")
+                let chatRoom: ChatRoom
+                if ChatController.rooms[room] == nil {
+                    ChatController.rooms[room] = ChatRoom()
+                }
+                chatRoom = ChatController.rooms[room]!
+                let asUser = ChatUser(username)
+                chatRoom.queue.send(.userJoined(user: asUser))
+                let subscription = chatRoom.queue.sink(receiveValue: { (event) in
+                    switch (event){
+                    case .privateMessage(let fromUser, let toUser, _):
+                        guard toUser == user || fromUser == user, let res = self.codableAsString(event) else {
+                            return // not for you!
+                        }
+                        print("Chat: sending private message to \(username)")
+                        ws.send(res)
+                    default:
+                        print("Chat: sending event to \(username)")
+                        if let res = self.codableAsString(event) {
+                            ws.send(res)
+                        }
+                    }
+                })
+                self.subscriptions.append(subscription)
+                let result: JoinRoomResponse = .success(room: room, username: username, membership: chatRoom.userCount)
+                guard let asString = self.codableAsString(result) else {
+                    return
+                }
+                ws.send(asString)
+                user = asUser
+                chatRoom.users.insert(asUser)
+                inRoom = room
+            case .sendMessage(let message):
+                self.sendMessage(message, ws: ws, user: &user, roomName: &inRoom)
+            }
+        }
     }
     
     // MARK: Helpers
