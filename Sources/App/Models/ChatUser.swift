6eca14fe70e10fa9fbdc71df1309c36bde873b95
grey280 <greypatterson@gmail.com>
2020-05-17 10:20:59 -0700

    Chat user can be system

--- Sources/App/Models/ChatUser.swift
+++ Sources/App/Models/ChatUser.swift
@@ -9,8 +9,14 @@ import Foundation
 
 final class ChatUser {
     let username: String
-    init(_ username: String){
+    let isSystem: Bool
+    init(_ username: String, isSystem: Bool = false){
         self.username = username
+        self.isSystem = isSystem
+    }
+    
+    static var system: ChatUser {
+        ChatUser("System", isSystem: true)
     }
 }
 
@@ -29,16 +35,20 @@ extension ChatUser: Codable {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(username, forKey: .username)
         try container.encode(hashValue, forKey: .id)
+        try container.encode(isSystem, forKey: .isSystem)
     }
     
     fileprivate enum CodingKeys: String, CodingKey {
         case username
         case id
+        case isSystem
     }
     
     convenience init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let username = try container.decode(String.self, forKey: .username)
+//        let isSystem = try container.decode(Bool.self, forKey: .isSystem)
+        // don't decode 'isSystem' - never comes in from network, so can never be decoded
         self.init(username)
     }
 }
