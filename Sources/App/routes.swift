07ccd9d2603e6d3f70028097f9d139d81a4bad00
grey280 <greypatterson@gmail.com>
2020-05-24 17:23:19 -0700

    Kick to lobby if no game ID set

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -18,6 +18,18 @@ func routes(_ app: Application) throws {
         let safeName = name.replacingOccurrences(of: "'", with: "")
         return req.view.render("lobby", User(username: safeName))
     }
+    
+    app.get("game") { req -> EventLoopFuture<View> in
+        var name: String? = req.query["username"]
+        if (name == nil || name == ""){
+            name = "Anonymous_\(Int.random(in: 1..<100))"
+        }
+        name = name!.replacingOccurrences(of: "'", with: "")
+        guard let game: GameConfig.ID = req.query["game"] else {
+            return req.view.render("lobby", User(username: name!))
+        }
+        return req.view.render("game", GameConfig(username: name!, gameID: game))
+    }
 
     app.get("hello") { req -> String in
         return "Hello, world!"
